9c00be592d86a632b06b9dc0d79b4fdb5db5e0fd
kc10kevin <kc10kevin@yahoo.com>
2016-02-08 17:44:08 -0600

    Delete stmloader_loader.c.o.d

--- Debug/stmloader_loader.c.o.d
+++
@@ -1,6 +0,0 @@
-Debug/stmloader_loader.c.o: support/stmloader/loader.c \
- support/stmloader/serial.h support/stmloader/stmbootloader.h
-
-support/stmloader/serial.h:
-
-support/stmloader/stmbootloader.h:
