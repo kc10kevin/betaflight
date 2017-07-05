564f012120d4d651f02b7df254293b49013c7635
kc10kevin <kc10kevin@yahoo.com>
2017-07-05 18:18:12 -0500

    Master Rebase 5 Jul

--- src/main/target/FURYF7/target.h
+++ src/main/target/FURYF7/target.h
@@ -24,12 +24,13 @@
 #define LED0_PIN                PB5
 #define LED1_PIN                PB4
 
-#define BEEPER                  PD10
+#define BEEPER                  PC13
 #define BEEPER_INVERTED
 
 #define USE_EXTI
 #define MPU_INT_EXTI            PC4
 #define USE_MPU_DATA_READY_SIGNAL
+//#define ENSURE_MPU_DATA_READY_IS_LOW
 
 #define ICM20689_CS_PIN          PA4
 #define ICM20689_SPI_INSTANCE    SPI1
@@ -54,11 +55,11 @@
 #define SPI1_MISO_PIN           PA6
 #define SPI1_MOSI_PIN           PA7
 
-//#define USE_SPI_DEVICE_2
-//#define SPI2_NSS_PIN            PB12
-//#define SPI2_SCK_PIN            PB13
-//#define SPI2_MISO_PIN           PB14
-//#define SPI2_MOSI_PIN           PB15
+#define USE_SPI_DEVICE_2
+#define SPI2_NSS_PIN            PB12
+#define SPI2_SCK_PIN            PB13
+#define SPI2_MISO_PIN           PB14
+#define SPI2_MOSI_PIN           PB15
 
 #define USE_SPI_DEVICE_3
 #define SPI3_NSS_PIN            PA15
@@ -66,48 +67,55 @@
 #define SPI3_MISO_PIN           PC11
 #define SPI3_MOSI_PIN           PC12
 
-#define USE_SPI_DEVICE_4
-#define SPI4_NSS_PIN            PE11
-#define SPI4_SCK_PIN            PE12
-#define SPI4_MISO_PIN           PE13
-#define SPI4_MOSI_PIN           PE14
-
 #define USE_SDCARD
+
 #define SDCARD_DETECT_INVERTED
-
 #define SDCARD_DETECT_PIN                   PD2
-#define SDCARD_DETECT_EXTI_LINE             EXTI_Line2
-#define SDCARD_DETECT_EXTI_PIN_SOURCE       EXTI_PinSource2
-#define SDCARD_DETECT_EXTI_PORT_SOURCE      EXTI_PortSourceGPIOD
-#define SDCARD_DETECT_EXTI_IRQn             EXTI2_IRQn
+//#define SDCARD_DETECT_EXTI_LINE             EXTI_Line2
+//#define SDCARD_DETECT_EXTI_PIN_SOURCE       EXTI_PinSource2
+//#define SDCARD_DETECT_EXTI_PORT_SOURCE      EXTI_PortSourceGPIOD
+//#define SDCARD_DETECT_EXTI_IRQn             EXTI2_IRQn
 
-#define SDCARD_SPI_INSTANCE                 SPI4
-#define SDCARD_SPI_CS_PIN                   SPI4_NSS_PIN
+#define SDCARD_SPI_INSTANCE                 SPI3
+#define SDCARD_SPI_CS_PIN                   PA15
 
-#define SDCARD_SPI_INITIALIZATION_CLOCK_DIVIDER 256 // 422kHz
+// SPI2 is on the APB1 bus whose clock runs at 84MHz. Divide to under 400kHz for init:
+#define SDCARD_SPI_INITIALIZATION_CLOCK_DIVIDER 256 // 328kHz
 // Divide to under 25MHz for normal operation:
-#define SDCARD_SPI_FULL_SPEED_CLOCK_DIVIDER 8 // 13.5MHz
+#define SDCARD_SPI_FULL_SPEED_CLOCK_DIVIDER     8 // 21MHz
 
-#define SDCARD_DMA_CHANNEL_TX               DMA2_Stream1
+#define SDCARD_DMA_CHANNEL_TX               DMA1_Stream7
 #define SDCARD_DMA_CHANNEL_TX_COMPLETE_FLAG DMA_FLAG_TCIF1_5
-#define SDCARD_DMA_CLK                      RCC_AHB1Periph_DMA2
-#define SDCARD_DMA_CHANNEL                  DMA_CHANNEL_4
+//#define SDCARD_DMA_CHANNEL_TX_COMPLETE_FLAG DMA_FLAG_TCIF5
+#define SDCARD_DMA_CLK                      RCC_AHB1Periph_DMA1
+#define SDCARD_DMA_CHANNEL                  DMA_CHANNEL_0
 
 #define USE_FLASHFS
 #define USE_FLASH_M25P16
-#define M25P16_CS_PIN           PA15
+#define M25P16_CS_PIN           PB3
 #define M25P16_SPI_INSTANCE     SPI3
 
+#define OSD
+#define USE_MAX7456
+#define MAX7456_SPI_INSTANCE    SPI2
+#define MAX7456_SPI_CS_PIN      SPI2_NSS_PIN
+#define MAX7456_SPI_CLK         (SPI_CLOCK_STANDARD*2)
+#define MAX7456_RESTORE_CLK     (SPI_CLOCK_FAST)
+
 #define USE_VCP
-#define VBUS_SENSING_PIN        PC5
+//#define VBUS_SENSING_PIN        PC5
 
 #define USE_UART1
 #define UART1_RX_PIN            PA10
 #define UART1_TX_PIN            PA9
 
 #define USE_UART3
-#define UART3_RX_PIN            PD9
-#define UART3_TX_PIN            PD8
+#define UART3_RX_PIN            PB11
+#define UART3_TX_PIN            PB10
+
+//#define USE_UART4
+//#define UART4_RX_PIN            PA1
+//#define UART4_TX_PIN            PA0
 
 #define USE_UART6
 #define UART6_RX_PIN            PC7
@@ -120,41 +128,39 @@
 
 #define USE_ESCSERIAL
 #define ESCSERIAL_TIMER_TX_HARDWARE 0 // PWM 1
-#define ESCSERIAL_TIMER_TX_PIN  PC9 // XXX Provisional (Hardware=0, PPM)
+#define ESCSERIAL_TIMER_TX_PIN  PB9 // XXX Provisional (Hardware=0, PPM)
 
 #define USE_I2C
+#define USE_I2C_PULLUP
 #define USE_I2C_DEVICE_1
 #define I2C_DEVICE              (I2CDEV_1)
-#define USE_I2C_PULLUP
 #define I2C1_SCL                PB6
 #define I2C1_SDA                PB7
 
 #define USE_ADC
 #define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
+#define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
 #define VBAT_ADC_PIN            PC1
 #define RSSI_ADC_PIN            PC2
 #define CURRENT_METER_ADC_PIN   PC3
 
 #define ENABLE_BLACKBOX_LOGGING_ON_SDCARD_BY_DEFAULT
 
-#undef LED_STRIP
+//#define LED_STRIP
 
 #define DEFAULT_RX_FEATURE      FEATURE_RX_SERIAL
 #define SERIALRX_PROVIDER       SERIALRX_SBUS
+#define DEFAULT_FEATURES		FEATURE_OSD
 
-// XXX To target maintainer:
-// USE_SPEKTRUM_BIND is enabled for this target, and it will use
-// RX (or TX if half-duplex) pin of the UART the receiver is connected.
-// If PB11 is critical for this target, please resurrect this line.
-//#define SPEKTRUM_BIND_PIN       PB11
+#define SPEKTRUM_BIND_PIN       PB11
 
 #define USE_SERIAL_4WAY_BLHELI_INTERFACE
 
 #define TARGET_IO_PORTA         0xffff
 #define TARGET_IO_PORTB         0xffff
 #define TARGET_IO_PORTC         0xffff
-#define TARGET_IO_PORTD         0xffff
-#define TARGET_IO_PORTE         0xffff
+//#define TARGET_IO_PORTD         0xffff
+#define TARGET_IO_PORTD         (BIT(2))
 
-#define USABLE_TIMER_CHANNEL_COUNT 5
-#define USED_TIMERS             ( TIM_N(2) | TIM_N(3) | TIM_N(8))
+#define USABLE_TIMER_CHANNEL_COUNT 6
+#define USED_TIMERS             ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(8))
--- src/main/target/FURYF7/target.mk
+++ src/main/target/FURYF7/target.mk
@@ -1,5 +1,8 @@
-F7X5XG_TARGETS += $(TARGET)
-FEATURES       += SDCARD VCP ONBOARDFLASH
+F7X2RE_TARGETS  += $(TARGET)
+FEATURES        += VCP SDCARD ONBOARDFLASH
 
 TARGET_SRC = \
-            drivers/accgyro/accgyro_spi_icm20689.c
+            drivers/accgyro/accgyro_spi_icm20689.c \
+            drivers/max7456.c
+#            drivers/barometer/barometer_ms5611.c \
+#            drivers/barometer/barometer_spi_ms5611.c
