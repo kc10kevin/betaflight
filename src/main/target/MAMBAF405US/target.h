00fb92cff3560df9b26df83957e212a7a753e9ce
Kevin Berner <kc10kevin@yahoo.com>
2020-09-26 11:20:59 -0500

    Config Changes

--- src/main/target/MAMBAF405US/target.h
+++ src/main/target/MAMBAF405US/target.h
@@ -20,10 +20,10 @@
 
 #pragma once
 
-//#define USE_TARGET_CONFIG
+#define USE_TARGET_CONFIG
 
-#define TARGET_BOARD_IDENTIFIER "MBF7"
-#define USBD_PRODUCT_STRING     "MAMBAF722"
+#define TARGET_BOARD_IDENTIFIER "MBF4"
+#define USBD_PRODUCT_STRING     "MAMBAF405US"
 
 // ******* LEDs and BEEPER ********
 
@@ -31,13 +31,15 @@
 #define LED1_PIN                PC14
 
 #define USE_BEEPER
-#define BEEPER_PIN              PB2
+#define BEEPER_PIN              PC13
 #define BEEPER_INVERTED
+
+#define INVERTER_PIN_UART1      PC0 // PC0 used as inverter select GPIO
 
 #define ENABLE_DSHOT_DMAR       DSHOT_DMAR_ON
 
-//#define USE_PINIO
-//#define PINIO1_PIN PB0 // Bluetooth mode control, PB0 is connected to the 36 pin (P2.0) of the Bluetooth chip. Replace PB0 with the pin for your flight control and 36-pin connection
+#define USE_PINIO
+#define PINIO1_PIN PB0 // Bluetooth mode control, PB0 is connected to the 36 pin (P2.0) of the Bluetooth chip. Replace PB0 with the pin for your flight control and 36-pin connection
 
 // ******* GYRO and ACC ********
 
@@ -56,12 +58,14 @@
 #define USE_ACC
 #define USE_ACC_SPI_MPU6000
 
+#define USE_ACCGYRO_BMI160
+#ifdef USE_ACCGYRO_BMI160
+#define BMI160_SPI_DIVISOR      16
+#endif
+
 // ******* SERIAL ********
 
 #define USE_VCP
-#define USE_USB_DETECT
-#define USB_DETECT_PIN          PC13
-
 #define USE_UART1
 #define USE_UART2
 #define USE_UART3
@@ -91,6 +95,22 @@
 #define USE_SOFTSERIAL2
 
 #define SERIAL_PORT_COUNT       9 //VCP, UART1-UART6 , 2 x Soft Serial
+
+#define USE_BARO
+#define USE_BARO_MS5611
+#define USE_BARO_BMP280
+//#define MS5611_I2C_INSTANCE     I2CDEV_1
+
+#define USE_MAG
+#define USE_MAG_HMC5883
+#define USE_MAG_QMC5883
+#define USE_MAG_LIS3MDL
+
+#define USE_I2C
+#define USE_I2C_DEVICE_2
+#define I2C_DEVICE              (I2CDEV_2)
+#define I2C2_SCL                NONE      
+#define I2C2_SDA                NONE
 
 // ******* SPI ********
 
