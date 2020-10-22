e1be9e8b103d6edb759a5d24174f7bb7a3141d2b
Kevin Berner <kc10kevin@yahoo.com>
2020-10-21 23:04:21 -0500

    MAMBA722 I2C

--- src/main/target/MAMBAF405US/target.mk
+++ src/main/target/MAMBAF405US/target.mk
@@ -1,12 +1,13 @@
-F7X2RE_TARGETS += $(TARGET)
+F405_TARGETS    += $(TARGET)
 FEATURES       += VCP ONBOARDFLASH
 
 TARGET_SRC = \
             drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_spi_mpu6000.c \
-			drivers/barometer/barometer_ms5611.c \
+            drivers/accgyro/accgyro_spi_bmi160.c \
+            drivers/barometer/barometer_ms5611.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/compass/compass_hmc5883l.c \
             drivers/compass/compass_qmc5883l.c \
             drivers/compass/compass_lis3mdl.c \
-           drivers/max7456.c
+            drivers/max7456.c
