43b12aaee3a9a2095043c511c671a3bbce2f7a6d
Kevin Berner <kc10kevin@yahoo.com>
2020-12-26 09:02:24 -0600

    Initial MAMBAF745

---
+++ src/main/target/MAMBAF745/target.mk
@@ -0,0 +1,8 @@
+F7X5XG_TARGETS += $(TARGET)
+FEATURES       += VCP ONBOARDFLASH
+
+TARGET_SRC = \
+            drivers/accgyro/accgyro_mpu.c \
+            drivers/accgyro/accgyro_spi_mpu6000.c \
+            drivers/barometer/barometer_bmp280.c \
+            drivers/max7456.c
